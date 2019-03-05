d11f884e1e2981d4943944901759f6b70a95ed56
Timo Holvoet <timo.holvoet@student.howest.be>
2019-03-05 08:54:01 +0100

    fixed typo

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 // #1 import van toegevoegde packages (via npm of yarn)
 
-// #2 De tasks / watchers zelf die we aanmaken. 
+// #2 De tasks / watchers zelf die we aanmaken.
+// a) we willen een development server opzetten
+
+// b) html moet geminified naar de dist-map opgezet worden
+
+// c) JS willen we samenvoegen, minifien en compatibel aanmaken
+
+// d) watchen van verandereingen
 
 // #3 Export van onze eigen tasks. (functions)
 // ps: dan kunnen we ze gebruiken via de command line.
